b683cbcb1165b6acff766e5537cd82082b456d5f
Cooper <davidontech@gmail.com>
2018-12-21 00:26:20 +1300

    bootz cmd

--- include/configs/larm_one.h
+++ include/configs/larm_one.h
@@ -6,7 +6,7 @@
 #define CONFIG_SYS_LOAD_ADDR                (CONFIG_SYS_SDRAM_BASE + 4 * 1024 * 1024)
 #define CONFIG_SYS_MALLOC_LEN               1 * 1024 * 1024
 
-#define CONFIG_SYS_INIT_RAM_SIZE            8 * 1024 * 1024
+#define CONFIG_SYS_INIT_RAM_SIZE            16 * 1024 * 1024
 #define CONFIG_SYS_INIT_SP_ADDR             (CONFIG_SYS_SDRAM_BASE + (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE))
 
 #endif
