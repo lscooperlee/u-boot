d21b872137a3af0703a1d031f824c7aed1a26483
Cooper <davidontech@gmail.com>
2019-01-13 23:08:13 +1300

    boot linux done

--- board/larm/larm_one/larm_one.c
+++ board/larm/larm_one/larm_one.c
@@ -1,10 +1,13 @@
 
+#include <asm/mach-types.h>
 #include <common.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
+    gd->bd->bi_arch_number = MACH_TYPE_LARM_ONE;
+    gd->bd->bi_boot_params = CONFIG_BOOT_PARAMS_ADDR;
 	return 0;
 }
 
--- include/configs/larm_one.h
+++ include/configs/larm_one.h
@@ -1,12 +1,14 @@
 #ifndef __LARM_ONE_CONFIG_H__
 #define __LARM_ONE_CONFIG_H__
 
+
 #define CONFIG_ENV_SIZE                     0x8000
 #define CONFIG_SYS_SDRAM_BASE               0
 #define CONFIG_SYS_LOAD_ADDR                (CONFIG_SYS_SDRAM_BASE + 4 * 1024 * 1024)
 #define CONFIG_SYS_MALLOC_LEN               1 * 1024 * 1024
 
-#define CONFIG_SYS_INIT_RAM_SIZE            16 * 1024 * 1024
+#define CONFIG_SYS_INIT_RAM_SIZE            1024 * 1024 * 1024
 #define CONFIG_SYS_INIT_SP_ADDR             (CONFIG_SYS_SDRAM_BASE + (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE))
 
+#define CONFIG_BOOT_PARAMS_ADDR             (CONFIG_SYS_SDRAM_BASE + 0x100)
 #endif
