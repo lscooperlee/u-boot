714cfd906c2403f902b911e9a517482b0918d153
Cooper <davidontech@gmail.com>
2020-02-28 22:47:15 +1300

    Update to new uboot

--- arch/arm/mach-larm/larm.c
+++ arch/arm/mach-larm/larm.c
@@ -3,4 +3,5 @@
 __attribute__((noreturn)) void reset_cpu(ulong addr __attribute__((unused)))
 {
     asm("mov pc, #0");
+    while(1); // for noreturn warning
 }
--- include/configs/larm_one.h
+++ include/configs/larm_one.h
@@ -2,7 +2,6 @@
 #define __LARM_ONE_CONFIG_H__
 
 
-#define CONFIG_ENV_SIZE                     0x8000
 #define CONFIG_SYS_SDRAM_BASE               0
 #define CONFIG_SYS_LOAD_ADDR                (CONFIG_SYS_SDRAM_BASE + 4 * 1024 * 1024)
 #define CONFIG_SYS_MALLOC_LEN               1 * 1024 * 1024
